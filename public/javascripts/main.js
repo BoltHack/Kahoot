95537ebf8040998f44959c8b583b798a9144d016
mrlyov <vpoghosyan>
2025-03-05 12:46:18 +0400

    update 1 - 05.03.2025

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -39,3 +39,25 @@ function checkCookie() {
     }
 }
 checkCookie();
+
+
+function createSnowflake() {
+    const snowflake = document.createElement("div");
+    snowflake.innerHTML = "❄";
+    snowflake.classList.add("snowflake");
+
+    const size = Math.random() * 10 + 10 + "px";
+    snowflake.style.fontSize = size;
+
+    snowflake.style.left = Math.random() * window.innerWidth + "px";
+    snowflake.style.animationDuration = Math.random() * 5 + 4 + "s";
+    snowflake.style.opacity = Math.random();
+
+    document.body.appendChild(snowflake);
+
+    setTimeout(() => {
+        snowflake.remove();
+    }, 5000);
+}
+
+setInterval(createSnowflake, 100);
