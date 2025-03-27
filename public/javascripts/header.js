6133f921925169af2ad41da5f31f893e2b48ab79
mrlyov <vpoghosyan>
2025-03-27 23:19:02 +0400

    update 2 - 27.03.2025

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -18,7 +18,7 @@ const myToken = localStorage.getItem('token');
 const userInfo = JSON.parse(localStorage.getItem('userInfo') || '{}');
 if (myToken){
     account.innerHTML = `
-<div class="dropdown" style="text-align: center">
+<div class="dropdown">
     <p class="ha dropdown-title" style="color: #ced4da; background-color: #1C2025;">${userInfo.name}</p>
     <div class="dropdown-content">
         <a onclick="logout();">${localeType === 'en' ? 'Sign Out' : 'Выйти'}</a>
@@ -62,3 +62,19 @@ function checkGamePath(){
     }
 }
 checkGamePath();
+
+const dropdownTitles = document.querySelectorAll('.dropdown-title');
+const dropdownContents = document.querySelectorAll('.dropdown-content');
+const dropdown = document.querySelectorAll('.dropdown');
+
+dropdown.forEach((title, index) => {
+    title.addEventListener('mouseover', () => {
+        dropdownContents[index].style.display = 'flex';
+        dropdownContents[index].style.transform = 'scaleY(1)';
+    });
+
+    title.addEventListener('mouseout', () => {
+        dropdownContents[index].style.display = 'none';
+        dropdownContents[index].style.transform = 'scaleY(0)';
+    });
+});
