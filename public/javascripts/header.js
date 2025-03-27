440da41b601cb1e7ec589bfa2c8ffb5ee06fb830
mrlyov <vpoghosyan>
2025-03-27 20:00:28 +0400

    update 1 - 27.03.2025

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -18,8 +18,8 @@ const myToken = localStorage.getItem('token');
 const userInfo = JSON.parse(localStorage.getItem('userInfo') || '{}');
 if (myToken){
     account.innerHTML = `
-<div class="dropdown">
-    <a class="ha dropdown-title" style="color: #ced4da; background-color: #1C2025;">${userInfo.name}</a>
+<div class="dropdown" style="text-align: center">
+    <p class="ha dropdown-title" style="color: #ced4da; background-color: #1C2025;">${userInfo.name}</p>
     <div class="dropdown-content">
         <a onclick="logout();">${localeType === 'en' ? 'Sign Out' : 'Выйти'}</a>
     </div>
