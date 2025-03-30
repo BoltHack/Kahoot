93a957fa9b8d8891aa3939038f36761b1861e8ee
mrlyov <vpoghosyan>
2025-03-30 19:21:36 +0400

    update 3 - 30.03.2025

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -50,148 +50,42 @@ class PostController {
     static redaction = async (req, res, next) => {
         try {
             const { game_id } = req.params;
-            const { game_name, game_access, max_online, game_expiresInSeconds, game_max_questions,
-                question_title0, question_title1, question_title2, question_title3, question_title4,
-                question_title0_question_1, question_title0_question_2, question_title0_question_3, question_title0_question_4, question_title0_correct_question,
-                question_title1_question_1, question_title1_question_2, question_title1_question_3, question_title1_question_4, question_title1_correct_question,
-                question_title2_question_1, question_title2_question_2, question_title2_question_3, question_title2_question_4, question_title2_correct_question,
-                question_title3_question_1, question_title3_question_2, question_title3_question_3, question_title3_question_4, question_title3_correct_question,
-                question_title4_question_1, question_title4_question_2, question_title4_question_3, question_title4_question_4, question_title4_correct_question
+            const {
+                game_name, game_access, max_online, game_expiresInSeconds, game_max_questions,
+                ...questions
             } = req.body;
 
             const updateFields = {};
 
             if (game_name) updateFields.game_name = game_name;
-            if (game_access) updateFields.game_access = game_access;
-            if (game_access) updateFields.expiresInMinutes = 60;
+            if (game_access) {
+                updateFields.game_access = game_access;
+                updateFields.expiresInMinutes = 60;
+            }
             if (max_online) updateFields["game_online.max_online"] = max_online;
             if (game_expiresInSeconds) updateFields.game_expiresInSeconds = game_expiresInSeconds;
             if (game_max_questions) updateFields.game_max_questions = game_max_questions;
 
-            if (question_title0) {
-                let base64Image = default_question_image;
-                if (req.files && req.files.question_image0) {
-                    const imageFile = req.files.question_image0;
-                    base64Image = imageFile.data.toString('base64');
+            for (let i = 0; i < 5; i++) {
+                const titleKey = `question_title${i}`;
+                if (questions[titleKey]) {
+                    let base64Image = default_question_image;
+                    const imageKey = `question_image${i}`;
+                    if (req.files && req.files[imageKey]) {
+                        base64Image = req.files[imageKey].data.toString('base64');
+                    }
+
+                    updateFields[`game_questions.${i}`] = {
+                        question_title: questions[titleKey],
+                        question_image: base64Image,
+                        question_1: { title: questions[`${titleKey}_question_1`] },
+                        question_2: { title: questions[`${titleKey}_question_2`] },
+                        question_3: { title: questions[`${titleKey}_question_3`] },
+                        question_4: { title: questions[`${titleKey}_question_4`] },
+                        correct_question: questions[`${titleKey}_correct_question`],
+                        question_number: i
+                    };
                 }
-                updateFields["game_questions.0"] = {
-                    question_title: question_title0,
-                    question_image: base64Image,
-                    question_1: {
-                        title: question_title0_question_1,
-                    },
-                    question_2: {
-                        title: question_title0_question_2,
-                    },
-                    question_3: {
-                        title: question_title0_question_3,
-                    },
-                    question_4: {
-                        title: question_title0_question_4,
-                    },
-                    correct_question: question_title0_correct_question,
-                    question_number: 0
-                };
-            }
-            if (question_title1) {
-                let base64Image = default_question_image;
-                if (req.files && req.files.question_image1) {
-                    const imageFile = req.files.question_image1;
-                    base64Image = imageFile.data.toString('base64');
-                }
-                updateFields["game_questions.1"] = {
-                    question_title: question_title1,
-                    question_image: base64Image,
-                    question_1: {
-                        title: question_title1_question_1,
-                    },
-                    question_2: {
-                        title: question_title1_question_2,
-                    },
-                    question_3: {
-                        title: question_title1_question_3,
-                    },
-                    question_4: {
-                        title: question_title1_question_4,
-                    },
-                    correct_question: question_title1_correct_question,
-                    question_number: 1
-                };
-            }
-            if (question_title2) {
-                let base64Image = default_question_image;
-                if (req.files && req.files.question_image2) {
-                    const imageFile = req.files.question_image2;
-                    base64Image = imageFile.data.toString('base64');
-                }
-                updateFields["game_questions.2"] = {
-                    question_title: question_title2,
-                    question_image: base64Image,
-                    question_1: {
-                        title: question_title2_question_1,
-                    },
-                    question_2: {
-                        title: question_title2_question_2,
-                    },
-                    question_3: {
-                        title: question_title2_question_3,
-                    },
-                    question_4: {
-                        title: question_title2_question_4,
-                    },
-                    correct_question: question_title2_correct_question,
-                    question_number: 2
-                };
-            }
-            if (question_title3) {
-                let base64Image = default_question_image;
-                if (req.files && req.files.question_image3) {
-                    const imageFile = req.files.question_image3;
-                    base64Image = imageFile.data.toString('base64');
-                }
-                updateFields["game_questions.3"] = {
-                    question_title: question_title3,
-                    question_image: base64Image,
-                    question_1: {
-                        title: question_title3_question_1,
-                    },
-                    question_2: {
-                        title: question_title3_question_2,
-                    },
-                    question_3: {
-                        title: question_title3_question_3,
-                    },
-                    question_4: {
-                        title: question_title3_question_4,
-                    },
-                    correct_question: question_title3_correct_question,
-                    question_number: 3
-                };
-            }
-            if (question_title4) {
-                let base64Image = default_question_image;
-                if (req.files && req.files.question_image4) {
-                    const imageFile = req.files.question_image4;
-                    base64Image = imageFile.data.toString('base64');
-                }
-                updateFields["game_questions.4"] = {
-                    question_title: question_title4,
-                    question_image: base64Image,
-                    question_1: {
-                        title: question_title4_question_1,
-                    },
-                    question_2: {
-                        title: question_title4_question_2,
-                    },
-                    question_3: {
-                        title: question_title4_question_3,
-                    },
-                    question_4: {
-                        title: question_title4_question_4,
-                    },
-                    correct_question: question_title4_correct_question,
-                    question_number: 4
-                };
             }
 
             if (Object.keys(updateFields).length > 0) {
@@ -209,6 +103,7 @@ class PostController {
             next(err);
         }
     };
+
 
     static deleteGame = async (req, res, next) => {
         try {
@@ -510,68 +405,26 @@ class PostController {
             if (newsTag) updateFields["tags"].push({ tagName: 'News' });
             if (errorsTag) updateFields["tags"].push({ tagName: 'Errors' });
 
+            function updateFilesProcess(title, content, imageKey, index) {
+                if (title) {
+                    let base64Image;
+                    if (req.files && req.files[imageKey]) {
+                        const imageFile = req.files[imageKey];
+                        base64Image = imageFile.data.toString('base64');
+                    }
+                    updateFields[`update.${index}`] = {
+                        title,
+                        image: base64Image,
+                        content
+                    };
+                }
+            }
 
-
-            if (title0) {
-                let base64Image;
-                if (req.files && req.files.image0) {
-                    const imageFile = req.files.image0;
-                    base64Image = imageFile.data.toString('base64');
-                }
-                updateFields["update.0"] = {
-                    title: title0,
-                    image: base64Image,
-                    content: content0
-                }
-            }
-            if (title1) {
-                let base64Image;
-                if (req.files && req.files.image1) {
-                    const imageFile = req.files.image1;
-                    base64Image = imageFile.data.toString('base64');
-                }
-                updateFields["update.1"] = {
-                    title: title1,
-                    image: base64Image,
-                    content: content1
-                }
-            }
-            if (title2) {
-                let base64Image;
-                if (req.files && req.files.image2) {
-                    const imageFile = req.files.image2;
-                    base64Image = imageFile.data.toString('base64');
-                }
-                updateFields["update.2"] = {
-                    title: title2,
-                    image: base64Image,
-                    content: content2
-                }
-            }
-            if (title3) {
-                let base64Image;
-                if (req.files && req.files.image3) {
-                    const imageFile = req.files.image3;
-                    base64Image = imageFile.data.toString('base64');
-                }
-                updateFields["update.3"] = {
-                    title: title3,
-                    image: base64Image,
-                    content: content3
-                }
-            }
-            if (title4) {
-                let base64Image;
-                if (req.files && req.files.image4) {
-                    const imageFile = req.files.image4;
-                    base64Image = imageFile.data.toString('base64');
-                }
-                updateFields["update.4"] = {
-                    title: title4,
-                    image: base64Image,
-                    content: content4
-                }
-            }
+            updateFilesProcess(title0, content0, 'image0', 0);
+            updateFilesProcess(title1, content1, 'image0', 1);
+            updateFilesProcess(title2, content2, 'image0', 2);
+            updateFilesProcess(title3, content3, 'image0', 3);
+            updateFilesProcess(title4, content4, 'image0', 4);
 
             console.log('news_id', news_id);
 
---
+++ public/javascripts/back.js
@@ -0,0 +1,60 @@
+function pages(){
+    const pageName = document.getElementById('pageName');
+    const page = window.location.pathname;
+
+    if (page.startsWith('/create-game')){
+        pageName.innerHTML = `
+<a class="between"> > </a>
+<a class="other-color">${localeType === 'en' ? 'Game creation' : 'Создание игры'}</a>`
+    } else if (page.startsWith('/redaction')) {
+        pageName.innerHTML = `
+            <a class="between"> > </a>
+            <a class="other-color">${localeType === 'en' ? 'Editor' : 'Редактор'}</a>`
+    }
+    else if (page.startsWith('/my-games')) {
+        pageName.innerHTML = `
+            <a class="between"> > </a>
+            <a class="other-color">${localeType === 'en' ? 'My games' : 'Мои игры'}</a>`
+    }
+    else if (page.startsWith('/friends')) {
+        pageName.innerHTML = `
+            <a class="between"> > </a>
+            <a class="other-color">${localeType === 'en' ? 'My friends' : 'Мои друзья'}</a>`
+    }
+    else if (page.startsWith('/support')) {
+        pageName.innerHTML = `
+            <a class="between"> > </a>
+            <a class="other-color">${localeType === 'en' ? 'Support' : 'Поддержка'}</a>`
+    }
+    else if (page.startsWith('/privacyPolicy')) {
+        pageName.innerHTML = `
+            <a class="between"> > </a>
+            <a class="other-color">${localeType === 'en' ? 'Privacy policy' : 'Политики конфиденциальности'}</a>`
+    }
+    else if (page.startsWith('/rules')) {
+        pageName.innerHTML = `
+            <a class="between"> > </a>
+            <a class="other-color">${localeType === 'en' ? 'Terms of use' : 'Политика использования'}</a>`
+    }
+    else if (page.startsWith('/aboutUs')) {
+        pageName.innerHTML = `
+            <a class="between"> > </a>
+            <a class="other-color">${localeType === 'en' ? 'About us' : 'О нас'}</a>`
+    }
+    else if (page.startsWith('/news')) {
+        pageName.innerHTML = `
+            <a class="between"> > </a>
+            <a class="other-color">${localeType === 'en' ? 'News' : 'Новости'}</a>`
+    }
+    else if (page.startsWith('/writeNews')) {
+        pageName.innerHTML = `
+            <a class="between"> > </a>
+            <a href="/news" class="color-btn">${localeType === 'en' ? 'News' : 'Новости'}</a>
+            <a class="between"> > </a>
+            <a class="other-color">${newsTitle}</a>`
+    }
+    else if (page === '/') {
+        document.getElementById('back').style.display = 'none';
+    }
+}
+pages();
