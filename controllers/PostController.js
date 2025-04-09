39ea625f1c944b8bbc5036c1643d2b8eba447bef
mrlyov <vpoghosyan>
2025-04-09 14:27:00 +0400

    update 2 - 09.04.2025

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -327,12 +327,11 @@ class PostController {
 
     static changeSettings = async (req, res, next) => {
         try {
-            const {notifications, soundTrack, mainEffects, locale} = req.body;
+            const {notifications, soundTrack, mainEffects} = req.body;
 
             res.cookie('notifications', notifications ? 'on' : 'off', { httpOnly: true, maxAge: 10 * 365 * 24 * 60 * 60 * 1000 });
             res.cookie('soundTrack', soundTrack ? 'on' : 'off', { httpOnly: true, maxAge: 10 * 365 * 24 * 60 * 60 * 1000 });
             res.cookie('mainEffects', mainEffects ? 'on' : 'off', { httpOnly: true, maxAge: 10 * 365 * 24 * 60 * 60 * 1000 });
-            res.cookie('locale', locale, { httpOnly: true, maxAge: 10 * 365 * 24 * 60 * 60 * 1000 });
 
             setTimeout(function () {
                 return res.redirect('/settings');
