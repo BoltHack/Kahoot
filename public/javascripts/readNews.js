cd808b2082f482fad53a7a49cbf286ad1e5c8cdc
BoltHack <bolthack228@gmail.com>
2025-10-17 21:39:49 +0400

    kahoot update 1 - 17.10.2025

--- public/javascripts/readNews.js
+++ public/javascripts/readNews.js
@@ -2,7 +2,7 @@ const stickyHeight = document.getElementById('stickyHeight');
 const body = document.body, html = document.documentElement;
 
 const height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
-stickyHeight.style.height = `calc(${height}px - 650px)`;
+stickyHeight.style.height = `calc(${height}px - 731px)`;
 
 
 
